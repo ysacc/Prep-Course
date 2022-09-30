f1e20bdce7227cf5445f84f8f4d6b3f121897e05
Ysacc Roncal <samironcal@gmail.com>
2022-09-30 15:56:34 -0500

    actual

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -16,7 +16,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 1.05;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,32 +28,33 @@ const nuevoModulo = 21 % 5 === 1.05;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  return str
+  return str;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  x + y
+  
+  return x + y;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  x - y
+  return x - y;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  x * y
+  return x * y;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  x / y
+  return x / y;
 }
 
 function sonIguales(x, y) {
@@ -138,7 +139,7 @@ function redondearNumero(num) {
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  return Math.round(num)
+  return Math.ceil(num)
 }
 
 function numeroRandom() {
@@ -152,50 +153,58 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if(numero>0) {return "Es positivo"}
-  else if (numero ==0){return false}
-  else return 'Es negativo '
+  if(numero===0) {
+    return false; 
+  }
+  else if (numero >0){
+    return "Es positivo";
+  }
+  else {
+    return "Es negativo";
+  }
 }
+
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return  str + "!"
+  return  str + "!";
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return nombre + " " + apellido
+  var combi= nombre + " " + apellido;
+  return combi;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "hola " + nombre + "!"
+  return ("Hola " + nombre + "!");
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  return alto * ancho
+  return alto * ancho;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  return lado * 4
+  return lado * 4;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-return ( base* altura)/2
+return ( base* altura)/2;
 }
 
 
@@ -203,7 +212,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  return euro * 1.20
+  return euro * 1.20;
 }
 
 
@@ -213,10 +222,15 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra == a,e,i,o,u){return "Es vocal"}
-  else if(letra.length >1){return "Dato incorrecto"}
-  else return "Dato incorrecto"
-}
+  if(letra.length >1){
+    return "Dato incorrecto";
+  }
+  if (letra === "a" ||letra === "e"||letra === "i"||letra === "o"||letra === "u"){ 
+    return "Es vocal";
+  }
+  return "Dato incorrecto";
+  }
+
 
 
 
--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -6,11 +6,11 @@ function obtenerMayor(x, y) {
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código: 
   if(x>y){
-      console.log(x);
+      return(x);
       }else if (y>x){
-        console.log(y);
+        return(y);
       }else if (y===x){
-        console.log(x);
+        return(x);
       }
   }
 
@@ -20,9 +20,9 @@ function mayoriaDeEdad(edad) {
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
   if(edad>=18){
-    console.log("Allowed")
+    return("Allowed")
   }else{
-    console.log("Not Allowed")
+    return("Not Allowed")
   }
 }
   
@@ -33,11 +33,11 @@ function conection(status) {
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
   if(status===1){
-    console.log("Online");
+    return("Online");
   }else if(status===2){
-    console.log("Away");
+    return("Away");
   }else{
-    console.log("Offline")
+    return("Offline")
   }
 }
 
@@ -49,13 +49,13 @@ function saludo(idioma) {
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
     if(idioma === "aleman"){
-        console.log("Guten Tag!")
+        return("Guten Tag!")
       }else if (idioma === "mandarin"){
-        console.log("Ni Hao!")
+        return("Ni Hao!")
       }else if (idioma === "ingles"){
-        console.log("Hello!")
+        return("Hello!")
       }else {
-        console.log("Hola!")
+        return("Hola!")
       }
     }
 
@@ -69,19 +69,19 @@ function colors(color) {
   //Usar el statement Switch.
   switch (color){
     case "blue":
-      console.log("This is blue");
+      return("This is blue");
       break;
     case "red":
-    console.log("This is red");
+      return("This is red");
     break;
     case "green":
-    console.log("This is green");
+      return("This is green");
     break;
     case "orange":
-    console.log("This is orange");
+      return("This is orange");
     break;
     default:
-    console.log("Color not found");
+      return("Color not found");
     break;
   }
 }
@@ -91,11 +91,11 @@ function esDiezOCinco(numero) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if(numero === 10){
-    console.log("true")
+    return("true")
   }else if(numero === 5){
-    console.log("true")}
+    return("true")}
     else{
-      console.log("false")
+      return("false")
     }
   }
 
@@ -105,9 +105,9 @@ function estaEnRango(numero) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if(numero > 20 && numero < 50){
-    console.log ("true");
+    return("true");
   }else{
-    console.log("false")
+    return"false")
   }
 }
 
@@ -120,9 +120,9 @@ function esEntero(numero) {
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
   if(Number.isInteger(numero)){
-    console.log("true")
+    return("true")
   }else{
-    console.log("false")
+    return("false")
   }
 }
 
@@ -132,13 +132,13 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
   if(numero%3 === 0 && numero%5 === 0){
-    console.log("fizzbuzz");
+    return("fizzbuzz");
   }else if(numero%5 === 0){
-    console.log("fizz")
+    return("fizz")
   }else if(numero%3 === 0){
-    console.log("fizz")
+    return("fizz")
   }else{
-    console.log(numero)
+    return(numero)
       }
     }
   
@@ -152,16 +152,16 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
   if(num1===0 || num2===0 || num3 ===0){
-    console.log("Error");
+    return("Error");
   }else if(num1>num2 && num1>num3 && num1>0){
-    console.log("Número 1 es mayor y positivo")
+    return("Número 1 es mayor y positivo")
   }else if(num1<0 || num2 <0 || num3 < 0) {
-    console.log("Hay negativos");
+    return("Hay negativos");
   }else if(num3 > num1 && num3 >num2){
     num3++
-    console.log(num3)
+    return(num3)
   }else{
-    console.log("false")
+    return("false")
   }
 }
 
@@ -172,13 +172,13 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos 
   if(numero===0 || numero ===1){
-    console.log("falso");
+    return("falso");
     }else if (numero%2 ===0){
-    console.log("falso");
+      return("falso");
     }else if(numero%numero ===0 && numero>1){
-          console.log("true");
+          return("true");
         }else {
-          console.log("falso");
+          return("falso");
     }
 }
 
@@ -187,9 +187,9 @@ function esVerdadero(valor){
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 if(valor === "true"){
-  console.log("Soy Verdadero");
+  return("Soy Verdadero");
 }else if(valor==="false"){
-  console.log("Soy Falso")
+  return("Soy Falso")
 }
 }
 
@@ -197,18 +197,18 @@ function tablaDelSeis(n){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  for(let a=0; a<11;a++){
-    console.log(a*6)
+  var seis=[0,6,12,18,24,30,36,42,48,54,60];
+console.log(seis);
   }
-}
+
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
   if(numero<1000 && numero>99 ){
-    console.log ("true");
+    return ("true");
   }else{
-    console.log("false");
+    return("false");
   }
   }
 
@@ -218,15 +218,13 @@ function doWhile(numero) {
   //Retornar el valor final.
   //Usar el bucle do ... while.
   let i=numero
-let a=numero + 40
-do{
-i=i+5;
-console.log(i);
-}while(i< a);
+  let a=numero + 40
+  do{
+  i=i+5
+  }while(i< a);
+  console.log(i)
 }
-
-
-
+  
 // No modificar nada debajo de esta línea
 // --------------------------------
 
